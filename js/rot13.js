0591928305d69903e4e035c509842412d533e586
salteax <46114448+salteax@users.noreply.github.com>
2019-02-27 23:44:21 +0100

    Kommentare und Quellcode von rot13.js wurden ergänzt und verbessert.

--- js/rot13.js
+++ js/rot13.js
@@ -6,21 +6,23 @@ function rot13(text){
 
   for (var i = 0; i < text.length; i++) {
     position_current = text.charCodeAt(i);                                      //in Variable "position_current" wird der Unicode-Wert des aktuellen Zeichens gespeichert
-    console.log(position_current);
     position_shifted = text.charCodeAt(i) + key;                                //in Variable "position_shifted" wird der Unicode-Wert des Zeichens nach Verschiebung mit dem Schlüssel gespeichert
 
-    if (position_shifted > 90 && position_shifted < 104){                       //sollte es sich um ein verschobenes Zeichen mit einem Unicode Wert im Bereich von 90-104 (Großbuchstabe) handeln
-      resolution.push(String.fromCharCode(96+position_current-122+13));         //beginnt die restliche Zählung wieder bei dem Großbuchstaben A und das zugehörige Zeichen des Unicode-Wertes wird zum String und dem Array "resolution" hinzugefügt
+    if ((position_current >= 65 && position_current <= 90) || (position_current >= 97 && position_current <= 122)) { //sollte es sich um ein Buchstaben aus dem Alphabet handeln (65-90 = Großbuchstaben und 97-122 = Kleinbuchstaben)
+      if (position_shifted > 90 && position_shifted < 104){                     //sollte es sich um ein verschobenes Zeichen mit einem Unicode Wert im Bereich von 90-104 (Großbuchstabe) handeln
+        resolution.push(String.fromCharCode(96+position_current-122+13));       //beginnt die restliche Zählung wieder bei dem Großbuchstaben A und das zugehörige Zeichen des Unicode-Wertes wird zum String und dem Array "resolution" hinzugefügt
+      }
+      else if (position_shifted > 122){                                         //sollte es sich um ein verschobenes Zeichen mit dem Unicode Wert größer als 122 handeln
+        resolution.push(String.fromCharCode(96+position_current-122+13));       //beginnt die restliche Zählung wieder bei dem Kleinbuchstaben a und das zugehörige Zeichen des Unicode-Wertes wird zum String und dem Array "resolution" hinzugefügt
+      }
+      else {
+        resolution.push(String.fromCharCode(position_shifted));                 //das durch den Schlüssel verschobene und zugehörige Zeichen des Unicode-Wertes wird zum String und dem Array "resolution" hinzugefügt
+      }
     }
-    else if ((position_current >= 0 && position_current <= 64) || (position_current >= 123 && position_current <= 126 || (position_current >= 127))) { //sollten sich die Zeichen im Bereich von 0-64, 123-126 oder im Bereich von 127 und größer (Sonderzeichen) befinden
-      resolution.push(String.fromCharCode(position_current));                                                                                          //werden diese nicht verschoben
+    else {                                                                      //sollte es sich um kein Buchstaben aus dem Alphabet handeln (sondern ein Sonderzeichen/Umlaut/etc.)
+      resolution.push(String.fromCharCode(position_current));                   //dann behält das Zeichen sein Wert und bleibt somit gleich (wird nicht verschoben)
     }
-    else if (position_shifted > 122){                                           //sollte es sich um ein verschobenes Zeichen mit dem Unicode Wert größer als 122 handeln
-      resolution.push(String.fromCharCode(96+position_current-122+13));         //beginnt die restliche Zählung wieder bei dem Kleinbuchstaben a und das zugehörige Zeichen des Unicode-Wertes wird zum String und dem Array "resolution" hinzugefügt
-    }
-    else {
-      resolution.push(String.fromCharCode(position_shifted));                   //das durch den Schlüssel verschobene und zugehörige Zeichen des Unicode-Wertes wird zum String und dem Array "resolution" hinzugefügt
-    }
+
   }
 
   return resolution.join('');                                                   //der Array "resolution" wird zum String zusammengefasst und zurückgegeben
