c1663875a58bfea74612288d95b8c74b8ca05c4c
salteax <46114448+salteax@users.noreply.github.com>
2019-02-28 10:43:36 +0100

    index.html formatiert. style.css ergänzt. submit.js verbessert/verkürzt.

--- js/submit.js
+++ js/submit.js
@@ -1,36 +1,20 @@
 function submit(val1, val2, text, key){
-  console.log(val1);
-  console.log(val2);
-  console.log(text);
-  console.log(key);
 
-  if (val1 == 1 && val2 ==1) {
-    document.translator.output.value = rot13(text);
-  }
-  else if (val2 == 1) {
-    document.translator.output.value = rot13(text);
-  }
-  else if (val1 == 1) {
-    document.translator.output.value = rot13(text);
-  }
-  else if (val1 == 2 && val2 == 0) {
-    if (key == "") {
-      key = "SCHLUESSEL";
-      document.keyform.key.value = key;
-    }
-    document.translator.output.value = vigenere(true, key, text)
-  }
-  else if (val1 == val2) {
-    document.translator.output.value = text;
-  }
-  else if (val2 == 2) {
-    if (key == "") {
-      key = "SCHLUESSEL";
-      document.keyform.key.value = key;
-    }
-    document.translator.output.value = vigenere(false, key, text)
-  }
-  else {
-    document.translator.output.value = "";
+  switch (val2) {
+    case '0':
+      document.textform.output.value = rot13(text);
+      break;
+    case '1':
+      if (key == "") {
+        key = "vigenere";
+        document.keyform.key.value = key;
+      }
+      if (val1 == 1) {
+        document.textform.output.value = vigenere(false,key,text);
+      } else {
+        document.textform.output.value = vigenere(true,key,text);
+      }
+      break;
+    default:
   }
 }
